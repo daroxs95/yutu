df23344ceeb62f20644af16a3beea57acc5fbd64
Darián López Utra <daroxs95@gmail.com>
2024-03-08 00:38:03 +0100

    Add `PLAYLISTS_API_ENDPOINT`
    - Hardcode env variables due startup issues in some systems

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,8 @@
-export const API_ENDPOINT = import.meta.env.VITE_API_URL.replace(/\/$/, '');
+// export const API_ENDPOINT = import.meta.env.VITE_API_URL.replace(/\/$/, '');
+// export const PLAYLISTS_API_ENDPOINT = import.meta.env.VITE_PLAYLIST_API_URL.replace(
+//   /\/$/,
+//   '',
+// );
+
+export const API_ENDPOINT = 'https://youtube.thorsteinsson.is/api';
+export const PLAYLISTS_API_ENDPOINT = 'https://yutu-be.daroxs95.workers.dev/api';
